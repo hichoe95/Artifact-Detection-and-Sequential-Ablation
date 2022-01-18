e4bfdb02125217b1413b62ce8129e1327cb391de
CHOI HWAN IL <hichoe95@naver.com>
2022-01-18 22:14:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from config import parse_args
 from model import pggan, stylegan2
-from misc import print_two_images, print_images
+from misc import print_two_images, print_images, image_mask
 from seq_abl import sequential_ablation
 
 import torch
@@ -28,9 +28,9 @@ def main(args):
 		original_image, repaired_image, mask_idx = sq.seq_abl(sample_idx = [1,34,123,341], layer_idx= [0,1,3], rate = '30', under = True)
 		masks = []
 		for i in range(len(mask_idx)):
-		    masks.append(image_mask(layer_idx = [0,1,3], act_idx = mask_idx[i], resolution = args.resolution, model = args.model))
+			masks.append(image_mask(layer_idx = [0,1,3], act_idx = mask_idx[i], resolution = args.resolution, model = args.model))
 		for i in range(original_image.size(0)):
-	    	print_two_images(original_image[[i]], repaired_image[[i]], masks[i], labels = ['origin', 'corrected'])
+			print_two_images(original_image[[i]], repaired_image[[i]], masks[i], labels = ['origin', 'corrected'])
 	
 
 	if args.detection:
